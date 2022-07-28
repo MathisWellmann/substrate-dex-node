6cc082ee7ed354b7b0c4b3633ef4e752f735082d
MathisWellmann <wellmannmathis@gmail.com>
2022-07-28 20:44:32 +0200

    renamed pallet-template to pallet-dex

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -44,7 +44,7 @@ pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_dex;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -262,8 +262,7 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_dex::Config for Runtime {
 	type Event = Event;
 }
 
@@ -282,8 +281,7 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		Dex: pallet_dex,
 	}
 );
 
