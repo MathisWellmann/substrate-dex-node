bb329f3dc8ed1a6461585bafe5c3e698867405d6
MathisWellmann <wellmannmathis@gmail.com>
2022-07-29 14:46:48 +0200

    added some more Events, Errors and Dispatchables to pallet-dex

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ It is based on the substrate-node-template.
 We all know the standard node-template README, so I'll just focus on whats new and different in this one.
 
 - Bumped substrate branch from polkadot-v0.9.26 to polkadot-v0.9.27, unlike standard substrate-node-template (as of 2022-07-28)
+- Relies on pallet-assets
--- pallets/dex/src/lib.rs
+++ pallets/dex/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::{Decode, Encode};
+use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{BoundedVec, RuntimeDebug, RuntimeDebugNoBound};
 pub use pallet::*;
 
@@ -30,6 +30,7 @@ pub type Market<T: Config> = (BaseAsset<T>, QuoteAsset<T>);
 
 /// Can either be the Base or Quote asset
 #[derive(RuntimeDebugNoBound, Clone, Eq, PartialEq, Encode, Decode, TypeInfo)]
+#[scale_info(skip_type_params(T))]
 pub enum BaseOrQuote<T: Config> {
 	Base(BaseAsset<T>),
 	Quote(QuoteAsset<T>),
@@ -39,10 +40,14 @@ pub enum BaseOrQuote<T: Config> {
 /// The String limit bound is coming from pallet_assets
 pub type StringProxy<T: Config> = BoundedVec<u8, T::StringLimit>;
 
+/// Contains information about a market in addition to BASE and QUOTE assets
+#[derive(RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct MarketInfo {}
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*, Blake2_128Concat};
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::config]
@@ -58,8 +63,9 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn markets)]
+	pub type Markets<T: Config> =
+		StorageMap<_, Blake2_128Concat, Market<T>, MarketInfo, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -80,15 +86,49 @@ pub mod pallet {
 		/// 2: The amount which has been added
 		///
 		LiquidityAdded(Market<T>, BaseOrQuote<T>, T::Balance),
+
+		/// A liquidity provider (maker) has been rewarded with some balance
+		///
+		/// # Fields:
+		/// 0: The account which received a payout
+		/// 1: The amount that has been payed out
+		///
+		LiquidityProviderRewarded(T::AccountId, T::Balance),
+
+		/// A liquidity taker has swapped an asset for another
+		///
+		/// # Fields:
+		/// 0: The taker account
+		/// 1: The market the swap happened on
+		/// 2: TODO:
+		/// 3: The amount that was used in the swap
+		///
+		AssetSwapped(T::AccountId, Market<T>, BaseOrQuote<T>, T::Balance),
 	}
 
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		/// The market already exists and cannot be created
+		MarketExists,
+
+		/// The market the user specified does not exist
+		MarketDoesNotExist,
+
+		/// The user does not have enough balance
+		NotEnoughBalance,
+	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Creates a new pool for a market if it does not exist already
+		///
+		/// # Arguments:
+		/// origin: The obiquitous origin of a transaction
+		/// base_asset: The BASE asset of the market
+		/// quote_asset: The QUOTE asset of the market
+		///
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
-		pub fn create_pool(
+		pub fn create_market_pool(
 			origin: OriginFor<T>,
 			base_asset: StringProxy<T>,
 			quote_asset: StringProxy<T>,
@@ -100,8 +140,20 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Allows the user to deposit liquidity to a pool,
+		/// allowing for rewards to be generated on the deposit.
+		///
+		/// # Arguments:
+		/// origin: The obiquitous origin of a transaction
+		/// boq: Whether the user deposits the BASE or QUOTE asset
+		/// amount: The amount to deposit
+		///
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn deposit_to_pool(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn deposit_to_pool(
+			origin: OriginFor<T>,
+			boq: BaseOrQuote<T>,
+			amount: T::Balance,
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
 			// TODO: update storage
@@ -109,6 +161,32 @@ pub mod pallet {
 			// TODO: Emit proper event.
 			// Self::deposit_event(Event::SomethingStored(something, who));
 
+			Ok(())
+		}
+
+		/// Allows the user to buy the BASE asset of a market
+		///
+		/// # Arguments
+		/// origin: The obiquitous origin of a transaction
+		/// market: The market in which the user wants to trade
+		/// amount: The amount of the QUOTE asset the user is willing to spend
+		///
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+		pub fn buy(origin: OriginFor<T>, market: Market<T>, amount: T::Balance) -> DispatchResult {
+			todo!();
+			Ok(())
+		}
+
+		/// Allows the user to sell the BASE asset of a market
+		///
+		/// # Arguments:
+		/// origin: The obiquitous origin of a transaction
+		/// market: The market in which the user wants to trade
+		/// amount: The amount of BASE asset the user wants to sell
+		///
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+		pub fn sell(origin: OriginFor<T>, market: Market<T>, amount: T::Balance) -> DispatchResult {
+			todo!();
 			Ok(())
 		}
 	}
